ad5856052c22297b2c0f9739ef59a276531a1bd1
Michael Zhao <michael.zhao@yale.edu>
2013-09-07 01:41:42 -0400

    Updated templates

--- view_templates/js/application.js
+++ view_templates/js/application.js
@@ -1,17 +1,36 @@
 $(document).ready(function() {
+	var movies = 10;
+
 	// Test function
 	$('.btn').click(function(){
-		console.log("Click");
+		console.log("Clicked a button.");
 	});
 
-	
-	$(function() {
-		$('body').mousewheel(function(event, delta) {
-			this.scrollLeft -= (delta * 30);
-			event.preventDefault();
-		});
+	// Turn vertical scrolling into horizontal.
+	$('body').mousewheel(function(event, delta) {
+		this.scrollLeft -= (delta * 30);
+		event.preventDefault();
 	});
+
+
+
+
+	// When hovering over movie, show information.
+
+	
+	for (var i = 0; i < movies; i++) {
+		showMovie(i);
+	}
 
 
 
 });
+
+function showMovie(movie_num) {
+	$('#movie' + movie_num).hover(function() {
+		$('#Info-movie' + movie_num).removeAttr('hidden');
+	},
+	function() {
+		$('#Info-movie' + movie_num).attr('hidden', '');
+	});
+}
