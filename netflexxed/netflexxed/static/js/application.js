ac1a5d384c3fa953dd12fa0c4c7e1488f0dd5c44
Michael Zhao <michael.zhao@yale.edu>
2013-09-07 13:01:26 -0400

    Added alternate scrolling

--- netflexxed/netflexxed/static/js/application.js
+++ netflexxed/netflexxed/static/js/application.js
@@ -1,17 +1,42 @@
 $(document).ready(function() {
-	// Test function
-	$('.btn').click(function(){
-		console.log("Click");
+	var scrolledLeft = false;
+	var scrolledTop = true
+
+	$(window).scroll(function(){
+		console.log(scrolledLeft);
+		
+
+		
+
+		if  ($(window).scrollTop() >= 900) {
+			if ($(window).scrollLeft() === 0) {
+				if (scrolledLeft) {
+					scrolledLeft = false;
+					$(window).scrollTop(899);
+					$('body').mousewheel(function(event, delta) {
+						this.scrollTop -= (delta * .2);
+					});
+				}
+				else {
+					scrolledLeft = true;
+					$(window).scrollLeft(1);
+
+				}
+				
+			}
+
+			else if ($(window).scrollLeft() > 0) {
+
+				$(function() {
+					$('#triangle').css({position: 'fixed', top: '0px', 'z-index': '60'});
+					$('#back').css({position: 'fixed', top: '-700px'});
+					$('#scroll-objects').css({'margin-top': '700px'});
+					$('body').mousewheel(function(event, delta) {
+						this.scrollLeft -= (delta * .3);
+						event.preventDefault();
+					});
+				});
+			}
+		}
 	});
-
-	
-	$(function() {
-		$('body').mousewheel(function(event, delta) {
-			this.scrollLeft -= (delta * 30);
-			event.preventDefault();
-		});
-	});
-
-
-
 });
