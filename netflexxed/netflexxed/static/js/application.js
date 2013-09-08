14bbf57e99ee639723398ef513d9c94051608f73
Michael Zhao <michael.zhao@yale.edu>
2013-09-07 22:04:44 -0400

    Resolved lightbox failure.

--- netflexxed/netflexxed/static/js/application.js
+++ netflexxed/netflexxed/static/js/application.js
@@ -12,36 +12,11 @@
 
 		function parallaxScroll() {
 			var scrolled = $(window).scrollTop();
-    $('.parallax-1').css('top',(0-(scrolled*.4))+'px');
-    $('.parallax-2').css('top',(0-(scrolled*.7))+'px');
-    $('.parallax-3').css('top', (0-(scrolled*.7))+'px');
+			$('.parallax-1').css('top',(0-(scrolled*.4))+'px');
+			$('.parallax-2').css('top',(0-(scrolled*.7))+'px');
+			$('.parallax-3').css('top', (0-(scrolled*.7))+'px');
 
 		}
-/*
-	$(function(){
-      //Keep track of last scroll
-      var lastScroll = 0;
-      $(window).scroll(function(event){
-          //Sets the current scroll position
-          var st = $(this).scrollTop();
-          //Determines up-or-down scrolling
-          if (st > lastScroll){
-             //Replace this with your function call for downward-scrolling
-		$("#scroll-movies").animate({top: "-=9px"}, 0);
-          }
-          else {
-             //Replace this with your function call for upward-scrolling
-		$("#scroll-movies").animate({top: "+=9px"}, 0);
-          }
-          //Updates scroll position
-          lastScroll = st;
-      });
-    });
-	*/
-
-	// Alternate scrolling.
-	
-
 
 
 	// Lightboxes
@@ -50,28 +25,22 @@
 	}
 
 	$('.x-button').click(function() {
-						$('#lightbox' + cur_lightbox).hide();	
+		$('#lightbox' + cur_lightbox).hide();	
 
-	})
+	});
 });
 
 	function toggleLightbox(movie_id) {
 		$('#movie' + movie_id).click(function() {
-			
 			for (var i = 0; i < num_movies; i++) {
 				$('#lightbox' + i).hide();	
 			}
-			
+
 			$('#lightbox' + movie_id).fadeIn(500);
 			cur_lightbox = movie_id;
-		// $('#shade').fadeIn(500);
-	});
+		});
 	}
 
 	function hideLightbox(movie_id) {
 		$('#lightbox' + movie_id).attr('hidden', '');
-	}
-	function animateMargins(movie_id) {
-		$('#movie' + movie_id).animate({marginLeft: '+=10px'}, 0);
-
 	}
