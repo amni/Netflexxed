f78409c4ef5d5decc41e6668e86acb565c4571ca
Matthew Ebeweber <mebeweber@gmail.com>
2013-09-07 21:10:44 -0400

    that's monayyy

--- netflexxed/wsgi.py
+++ netflexxed/wsgi.py
@@ -24,7 +24,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "netflexxed.settings")
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
-application = Clint(get_wsgi_application())
+application = Cling(get_wsgi_application())
 
 
 
